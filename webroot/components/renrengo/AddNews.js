1ad6859ee3c55af0e50e197d6ebe1321bd5a7a51
zhouxudong <z_zxdong@163.com>
2016-10-26 22:16:40 +0800

    a " "

---
+++ webroot/components/renrengo/AddNews.js
@@ -0,0 +1,50 @@
+import React from "react"
+import {BreadCrumb, IBoxTool, TreeNode} from '../../common/Birdie'
+
+const AddNews = React.createClass({
+
+    render(){
+        var crumbs = [
+            {name: "新闻管理", url:"#"},
+            {name: "新闻添加", url: "#"}
+        ]
+        return (
+            <div className="addNew">
+                <BreadCrumb crumbs={crumbs} title="新闻管理"/>
+                <div className="wrapper wrapper-content animated fadeInRight">
+                    <div className="row">
+                        <IBoxTool title="新闻添加">
+                        <form className="form-horizontal" ref="addNewForm">
+                            <div className="form-group">
+                                <label className="control-label col-sm-2">新闻标题：</label>
+                                <div className="col-sm-8">
+                                    <input className="form-control" name="title" />
+                                </div>
+                            </div>
+                            <div className="form-group">
+                                <label className="control-label col-sm-2">标签：</label>
+                                <div className="col-sm-8">
+                                    <input className="form-control" name="tags" />
+                                </div>
+                            </div>
+                            <div className="form-group">
+                                <label className="control-label col-sm-2">新闻内容：</label>
+                                <div className="col-sm-8">
+                                    <textarea name="content" style={{width: "100%", height: "180px"}}></textarea>
+                                </div>
+                            </div>
+                            <div className="form-group">
+                                <div className="col-sm-4">
+                                    <button className="btn btn-primary">确定添加</button>
+                                </div>
+                            </div>
+                        </form>
+                        </IBoxTool>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+})
+
+export default AddNews;
--- webroot/components/renrengo/NewsManager.js
+++ webroot/components/renrengo/NewsManager.js
@@ -1,11 +1,49 @@
 import React from 'react'
+import {BreadCrumb, IBoxTool, TreeNode} from '../../common/Birdie'
 
 const NewsManager = React.createClass({
 
     render(){
+        var crumbs = [
+            {name: "新闻管理", url:"#"},
+            {name: "新闻列表", url: "#"}
+        ]
         return (
-            <div>
-                新闻管理
+            <div className="newmanager">
+                <BreadCrumb crumbs={crumbs} title="新闻管理"/>
+                <div className="wrapper wrapper-content animated fadeInRight">
+                    <div className="row">
+                        <div className="panel panel-info">
+                            <div className="panel-heading">
+                                新闻列表
+                            </div>
+                            <div className="panel-body">
+                                <table className="table table-bordered">
+                                    <thead>
+                                    <tr>
+                                        <th width="40%">新闻标题</th>
+                                        <th width="20%">标签</th>
+                                        <th width="20%">创建时间</th>
+                                        <th width="20%">操作</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <td>墨西哥商会宗旨</td>
+                                        <td>机械、大</td>
+                                        <td>2016-10-25 10:23</td>
+                                        <td>
+                                            <a href="#">编辑</a> | {" "}
+                                            <a href="#">发布</a> | {" "}
+                                            <a href="#">下架</a>
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
