04b86504f5cdb6724a62a0bbc2d4dc1b5b72d765
zhouxudong <z_zxdong@163.com>
2016-10-27 19:53:42 +0800

    新闻管理view“

--- webroot/components/renrengo/AddNews.js
+++ webroot/components/renrengo/AddNews.js
@@ -1,20 +1,107 @@
 import React from "react"
 import {BreadCrumb, IBoxTool, TreeNode} from '../../common/Birdie'
+import API from "../../conf/API"
 
 const AddNews = React.createClass({
 
+    contextTypes: {
+        router: React.PropTypes.object
+    },
     ue: null,
+    getInitialState(){
+        return {
+            newsInfo: {
+                title: "",
+                tags: "",
+                content: "新闻内容"
+            }
+        }
+    },
+    getNewsInfo(id){
+        return new Promise( (resolve, reject) => {
+            $.ajax({
+                url: API.news_info,
+                data: {
+                    id: id
+                },
+                success: function(data){
+                    if(!data.error_code){
+                        data = data.response_data;
+                        resolve(data);
+                    }else{
+                        alert(data.error_msg);
+                        reject(data);
+                    }
+                }.bind(this),
+                error: function(x){
+                    reject(x)
+                }
+            })
+        })
+    },
     componentDidMount(){
         var {container} = this.refs;
-        this.ue = UE.getEditor(container);
+        var {query} = this.props.location;
+        var id = query.id;
+        if(id){
+            this.getNewsInfo(id)
+            .then( data => {
+                this.setState({
+                    newsInfo: data
+                })
+                UE.delEditor(container);
+                this.ue = UE.getEditor(container);
+            })
+        }else{
+            UE.delEditor(container);
+            this.ue = UE.getEditor(container);
+        }
+
 
     },
     addNews(){
         var {addNewForm} = this.refs;
-        var postdata = $(addNewForm).serializeArray()
 
+        var postdata = $(addNewForm).serializeArray();
+        console.log(postdata);
+        $.ajax({
+            url: API.news_add,
+            data: postdata,
+            success: function(data){
+                if(!data.error_code){
+                    alert("添加成功！");
+                    this.context.router.push("/newsmgr/list")
+                }else{
+                    alert(data.error_msg);
+                }
+            }.bind(this),
+            error: function(x,s,t){
+                throw x;
+            }
+        })
+    },
+    updateNews(){
+        var {addNewForm} = this.refs;
+        var {query} = this.props.location;
+        var id = query.id;
+        var postdata = $(addNewForm).serializeArray();
+        $.ajax({
+            url: API.new_edit + "?id=" + id,
+            data: postdata,
+            success: function(data){
+                if(!data.error_code){
+                    alert("修改成功！");
+                    this.context.router.push("/newsmgr/list")
+                }else{
+                    alert(data.error_msg);
+                }
+            }.bind(this)
+        })
     },
     render(){
+        var {newsInfo} = this.state;
+        var {query} = this.props.location;
+        var id = query.id;
         var crumbs = [
             {name: "新闻管理", url:"#"},
             {name: "新闻添加", url: "#"}
@@ -29,24 +116,28 @@ const AddNews = React.createClass({
                             <div className="form-group">
                                 <label className="control-label col-sm-2">新闻标题：</label>
                                 <div className="col-sm-8">
-                                    <input className="form-control" name="title" />
+                                    <input onChange={this.handleNewsChange} className="form-control" name="title" value={newsInfo.title} />
                                 </div>
                             </div>
                             <div className="form-group">
                                 <label className="control-label col-sm-2">标签：</label>
                                 <div className="col-sm-8">
-                                    <input className="form-control" name="tags" />
+                                    <input onChange={this.handleNewsChange} className="form-control" name="tags" value={newsInfo.tags}/>
                                 </div>
                             </div>
                             <div className="form-group">
                                 <label className="control-label col-sm-2">新闻内容：</label>
                                 <div className="col-sm-8">
-                                    <textarea defaultValue="新闻内容" ref="container" name="content" style={{width: "100%", height: "180px"}}></textarea>
+                                    <textarea onChange={this.handleNewsChange} value={newsInfo.content} ref="container" name="content"></textarea>
                                 </div>
                             </div>
                             <div className="form-group">
                                 <div className="col-sm-4">
-                                    <button type="button" onClick={this.addNews} className="btn btn-primary">确定添加</button>
+                                    {
+                                        id ? <button type="button" onClick={this.updateNews} className="btn btn-primary">确定修改</button>
+                                            :<button type="button" onClick={this.addNews} className="btn btn-primary">确定添加</button>
+
+                                    }
                                 </div>
                             </div>
                         </form>
@@ -55,6 +146,17 @@ const AddNews = React.createClass({
                 </div>
             </div>
         )
+    },
+    handleNewsChange(e){
+        var $ele = e.target;
+        var name = $ele.name;
+        var value = $ele.value;
+        var {newsInfo} = this.state;
+        this.setState({
+            newsInfo : Object.assign({},newsInfo,{
+                [name] : value
+            })
+        })
     }
 })
 
--- webroot/components/renrengo/NewsManager.js
+++ webroot/components/renrengo/NewsManager.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import { Link } from "react-router"
 import {BreadCrumb, IBoxTool, TreeNode} from '../../common/Birdie'
+import API from "../../conf/API"
+import moment from "moment"
 
 const NewsManager = React.createClass({
 
+    getInitialState(){
+        return {
+            newsArr: []
+        }
+    },
+    componentDidMount(){
+        $.ajax({
+            url: API.news_list,
+            success: function(data){
+                if(!data.error_code){
+                    data = data.response_data;
+                    this.setState({
+                        newsArr: data
+                    })
+                }else{
+                    alert(data.error_msg);
+                }
+            }.bind(this)
+        })
+    },
     render(){
+        var {newsArr} = this.state;
         var crumbs = [
             {name: "新闻管理", url:"#"},
             {name: "新闻列表", url: "#"}
@@ -28,16 +52,22 @@ const NewsManager = React.createClass({
                                     </tr>
                                     </thead>
                                     <tbody>
-                                    <tr>
-                                        <td>墨西哥商会宗旨</td>
-                                        <td>机械、大</td>
-                                        <td>2016-10-25 10:23</td>
-                                        <td>
-                                            <a href="#">编辑</a> | {" "}
-                                            <a href="#">发布</a> | {" "}
-                                            <a href="#">下架</a>
-                                        </td>
-                                    </tr>
+                                    {
+                                        newsArr.map( news => {
+                                            return (
+                                                <tr key={news.id}>
+                                                    <td>{news.title}</td>
+                                                    <td>{news.tags}</td>
+                                                    <td>{moment(news.ctime).format("YYYY-MM-DD HH:mm")}</td>
+                                                    <td>
+                                                        <Link to={"/newsmgr/add?id=" + news.id}>编辑</Link> | {" "}
+                                                        <a href="#">发布</a> | {" "}
+                                                        <a href="#">下架</a>
+                                                    </td>
+                                                </tr>
+                                            )
+                                        })
+                                    }
                                     </tbody>
                                 </table>
                             </div>
